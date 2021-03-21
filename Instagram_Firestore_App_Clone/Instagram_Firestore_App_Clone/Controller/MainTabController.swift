23faaf6c533b72f3921d1c3d0852c97e772700b6
YounByoungil <jimmy7525@naver.com>
2021-03-21 17:30:52 +0900

    Configure Project

---
+++ Instagram_Firestore_App_Clone/Instagram_Firestore_App_Clone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  Instagram_Firestore_App_Clone
+//
+//  Created by 윤병일 on 2021/03/21.
+//
+
+import UIKit
+
+class MainTabController : UITabBarController {
+  
+  
+  //MARK: - Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
---
+++ Instagram_Firestore_App_Clone/Instagram_Firestore_App_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_Firestore_App_Clone
+//
+//  Created by 윤병일 on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+
+
+}
+
