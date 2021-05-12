68982fbab9e7230c3a5936a7f1fb09ca59a5a93d
YounByoungil <jimmy7525@naver.com>
2021-05-12 23:16:07 +0900

    Loading Indicator For Post Upload

--- Instagram_Firestore_App_Clone/Instagram_Firestore_App_Clone/Utils/Extensions.swift
+++ Instagram_Firestore_App_Clone/Instagram_Firestore_App_Clone/Utils/Extensions.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 extension UIButton {
   func attributedTitle(firstString : String, secondString : String) {
@@ -26,5 +27,17 @@ extension UIViewController {
     gradient.locations = [0, 1]
     view.layer.addSublayer(gradient)
     gradient.frame = view.frame
+  }
+  
+  static let hud = JGProgressHUD(style: .dark)
+  
+  func showLoader(_ show : Bool) {
+    view.endEditing(true)
+    
+    if show {
+      UIViewController.hud.show(in: view)
+    } else {
+      UIViewController.hud.dismiss()
+    }
   }
 }
